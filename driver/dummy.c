b87d906a6f3903286ef9c7ef50fd336dfa46d893
NanyaTK <herenfuyong02@gmail.com>
2023-09-07 22:21:10 +0900

    feat: step2 step2までを実装

--- driver/dummy.c
+++ driver/dummy.c
@@ -1,17 +1,25 @@
 #include <stddef.h>
 #include <stdint.h>
-#include <stdio.h>
 
 #include "net.h"
+#include "platform.h"
 #include "util.h"
 
 #define DUMMY_MTU UINT16_MAX /* maximum size of IP datagram */
+
+#define DUMMY_IRQ INTR_IRQ_BASE
 
 static int dummy_transmit(struct net_device *dev, uint16_t type,
                           const uint8_t *data, size_t len, const void *dst) {
     debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, len);
     debugdump(data, len);
     /* drop data */
+    intr_raise_irq(DUMMY_IRQ);
+    return 0;
+}
+
+static int dummy_isr(unsigned int irq, void *id) {
+    debugf("irq=%u, dev=%s", irq, ((struct net_device *)id)->name);
     return 0;
 }
 
@@ -23,7 +31,7 @@ struct net_device *dummy_init(void) {
     struct net_device *dev;
 
     dev = net_device_alloc();
-    if(!dev){
+    if (!dev) {
         errorf("net_device_alloc() faulure");
         return NULL;
     }
@@ -32,10 +40,11 @@ struct net_device *dummy_init(void) {
     dev->hlen = 0; /* non header */
     dev->alen = 0; /* non address */
     dev->ops = &dummy_ops;
-    if(net_device_register(dev)==-1){
+    if (net_device_register(dev) == -1) {
         errorf("net_device_register() failure");
         return NULL;
     }
+    intr_request_irq(DUMMY_IRQ, dummy_isr, INTR_IRQ_SHARED, dev->name, dev);
     debugf("initialized, dev=%s", dev->name);
     return dev;
 }
